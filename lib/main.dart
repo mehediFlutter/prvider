b3b9b7b61524d3dadea3515f0850b4d98e6fb967
mehediFluttere <mehedii.edu.bd@gmail.com>
2024-09-30 18:46:32 +0600

    provider example

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:provider_state_management/counter_example.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CounterExample();
+  }
+}
